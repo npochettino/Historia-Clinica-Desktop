d5c597bdcec1565d5fcdd87ab7745056803715b5
Nicolas Pochettino <sistemas.sempait@gmail.com>
2015-03-10 12:42:17 -0300

    add ManejoNHibernate.cs

---
+++ BibliotecaHistorialMedico/ClasesComplementarias/ManejoNHibernate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+
+namespace BibliotecaHistorialMedico.ClasesComplementarias
+{
+    class ManejoNHibernate
+    {
+        private static ISessionFactory CrearSesion()
+        {
+            ISessionFactory _sessionFactory = Fluently.Configure()
+              .Database(MsSqlConfiguration.MsSql2008.ConnectionString("data source=localhost;initial catalog=HistorialMedico;User=sa;Password=ana"))
+              .Mappings(m => m.FluentMappings.AddFromAssemblyOf<ModuloPrueba>())
+              .BuildSessionFactory();
+            return _sessionFactory;
+        }
+
+        internal static ISession IniciarSesion()
+        {
+            return CrearSesion().OpenSession();
+        }
+    }
+}
